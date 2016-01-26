f4846cc22802c66f51d468c6eb0315905da9c9d9
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2016-01-26 21:20:59 +0100

    d802: Build PAC

--- pac.mk
+++ pac.mk
@@ -1,18 +1,18 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common PAC stuff
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/pac/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d802/d802.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d802
-PRODUCT_NAME := cm_d802
+PRODUCT_NAME := pac_d802
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D802
-PRODUCT_MANUFACTURER := lge
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=lge/g2_open_com/g2:5.0.2/LRX22G/151061918340a:user/release-keys \
